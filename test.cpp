72b00e19318158fce6d4577df86303937b0a7866
Casper8126 <ca_project@CUDA.(none)>
2023-06-03 14:40:59 +0800

    test

--- test.cpp
+++ test.cpp
@@ -1,10 +1,11 @@
 //token
-//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 
+//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
 #include<iostream>
 
 int main(){
 	std::cout<<"recover OK"<<std::endl;
-	
+	int a = 10000;
+	std::cout<<a<<std::endl;
 	
 return 0;
 }
